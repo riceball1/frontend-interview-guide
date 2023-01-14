f706469718b58617c62e629caef5fb073cd89295
Dana Ng <14253024+riceball1@users.noreply.github.com>
2023-01-14 00:25:45 -0800

    Update setup (#4)
    * Update
    
    * Update

---
+++ pages/system.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+     <Header/>
+      <CBox
+        d="flex"
+        flex-dir="column"
+        justify-content="center"
+      >
+      FE System
+      </CBox>
+      <Footer/>
+    </div>
+  </template>
+  
+  <script lang="js">
+  import { CBox, CButton, CFlex, CHeading } from "@chakra-ui/vue";
+  
+  export default {
+    name: "system",
+    components: {
+      CBox,
+      CButton,
+      CFlex,
+      CHeading,
+    }
+  }
+ 
+  </script>
+  
