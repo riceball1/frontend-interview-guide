4667050d024f8d6738dded08a17427a583da3f43
Dana Ng <ngdana1@gmail.com>
2023-01-14 00:25:07 -0800

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,10 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/frontend-interview-guide/'
+  }
+  ,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Frontend Interview Guide',
