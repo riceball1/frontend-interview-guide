343904c089ddfd5eb284a33268463d9b2cb1cf3d
Dana Ng <14253024+riceball1@users.noreply.github.com>
2023-01-15 01:37:53 -0800

    Fix hosting (#15)
    * change nuxt config
    
    * update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,
-  nitro: {
-    preset: 'service-worker'
-  },
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   tailwindcss: {
     cssPath: '~/assets/css/main.css',
