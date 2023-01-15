11480b8d8bd6d8889ce0febd30473d7657c27892
Dana Ng <ngdana1@gmail.com>
2023-01-15 01:13:43 -0800

    update gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  target: 'static',
+  router: {
+    base: '/frontend-interview-guide/'
+  },
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   tailwindcss: {
     cssPath: '~/assets/css/main.css',
-  }
+  },
 })
