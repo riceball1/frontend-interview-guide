e7fc07528526b469c86097cb42c6c421ca9241d8
Dana Ng <ngdana1@gmail.com>
2023-01-15 01:17:34 -0800

    update nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'static',
-  router: {
-    base: '/frontend-interview-guide/'
-  },
+  ssr: false,
+  nitro: {
+    preset: 'service-worker'
+  }
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   tailwindcss: {
     cssPath: '~/assets/css/main.css',
