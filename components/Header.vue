ba109b1963449f40b79de986dd8cc7cbc5cc02de
Dana Ng <ngdana1@gmail.com>
2023-01-14 18:03:48 -0800

    Setup routing

--- components/Header.vue
+++ components/Header.vue
@@ -1,25 +1,31 @@
 <template>
-    <header>
-      <CBox p="10">
-        <CHeading as="h1" text-align="center" m="4">
-          ⚡️ Frontend Interview Guide
-        </CHeading>
-      </CBox>
-    </header>
-  </template>
-  
-  <script lang="js">
-  import { CBox, CButton, CFlex, CHeading } from "@chakra-ui/vue";
-  
-  export default {
-    name: "Header",
-    components: {
-      CBox,
-      CButton,
-      CFlex,
-      CHeading,
-    },
+  <header>
+    <CBox p="10">
+      <CHeading as="h1" text-align="center" m="4">
+        ⚡️ Frontend Interview Guide
+      </CHeading>
+      <NuxtLink to="/" v-if="!disableBackButton"> Go to Main Page </NuxtLink>
+    </CBox>
+  </header>
+</template>
 
-  };
-  </script>
-  
+<script lang="js">
+import { CBox, CButton, CFlex, CHeading } from "@chakra-ui/vue";
+
+export default {
+  name: "Header",
+  components: {
+    CBox,
+    CButton,
+    CFlex,
+    CHeading,
+  },
+  props: {
+    disableBackButton: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+};
+</script>
